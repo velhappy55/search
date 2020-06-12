0bea44974b74a2f998e29a92da217f6d29f06526
velhappy55 <velhappy55@gmail.com>
2020-06-12 22:20:56 +0530

    Updates

---
+++ precache-manifest.8453ffe1f95e79bb91a31825c936825a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "601541c27ee410c9cd3341dce07a401a",
+    "url": "/search/index.html"
+  },
+  {
+    "revision": "437c59119ba53d34de4b",
+    "url": "/search/static/css/2.14fbd4c8.chunk.css"
+  },
+  {
+    "revision": "de3d77db749057e39abb",
+    "url": "/search/static/css/main.fff6d848.chunk.css"
+  },
+  {
+    "revision": "437c59119ba53d34de4b",
+    "url": "/search/static/js/2.76589a6a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/search/static/js/2.76589a6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de3d77db749057e39abb",
+    "url": "/search/static/js/main.98985be7.chunk.js"
+  },
+  {
+    "revision": "0e60758145a8e3e2d96d",
+    "url": "/search/static/js/runtime-main.f4beeca9.js"
+  }
+]);
